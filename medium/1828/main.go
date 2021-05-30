da2f892a12af0f1b10f94a96269c324bf32c9aa5
asdfadsf <adsfad>
2021-05-30 13:45:39 +0600

    first task to github

---
+++ medium/1828/main.go
@@ -0,0 +1,16 @@
+func countPoints(points [][]int, queries [][]int) []int {
+	var dist = 0.0
+	ans := 0
+	var res []int
+	for _, query := range queries {
+		ans = 0
+		for _, point := range points {
+			dist = math.Sqrt(float64((query[0]-point[0])*(query[0]-point[0]) + (query[1]-point[1])*(query[1]-point[1])))
+			if dist <= float64(query[2]) {
+				ans++
+			}
+		}
+		res = append(res, ans)
+	}
+	return res
+}
