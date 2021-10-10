5f2b3a095faa7ce6758c78ae62deabec5d9a565e
sungatuly22 <sungatsson22@gmail.com>
2021-10-10 16:18:41 +0600

    some new tasks

---
+++ easy/0167/main.go
@@ -0,0 +1,13 @@
+//link for the task ---> https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+
+func twoSum(numbers []int, target int) []int {
+	l, r := 0, len(numbers)-1
+	for numbers[l]+numbers[r] != target {
+		if numbers[l]+numbers[r] > target {
+			r--
+		} else {
+			l++
+		}
+	}
+	return []int{l + 1, r + 1}
+}
